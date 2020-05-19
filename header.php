f67bd6508f9d2766734131e5fc0a66c974f58d94
ducthinh1606 <65561829+ducthinh1606@users.noreply.github.com>
2020-05-19 22:37:39 +0700

    1

---
+++ header.php
@@ -0,0 +1,45 @@
+<?php
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+?>
+
+<html>
+<head>
+    <title>Countdown Website</title>
+    <link rel="icon" href="images/muonline.gif"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script>
+        $(function() {
+            $(".imgDisplay").click(function() {
+                $(this).hide();
+            });
+        });
+
+        $(function() {
+            $(".refresh").click(function() {
+                $(this).show();
+            });
+        });
+    </script>
+</head>
+
+<body>
+
+<div class="container">
+    <div id="header">
+        <nav class="navbar navbar-inverse">
+            <div class="navbar-header">
+                <a href="" class="navbar-brand">Mu Blue</a>
+            </div>
+
+            <div class="navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="roomy.php">Roomy event</a></li>
+                    <li><a href="">Maze</a></li>
+                    <li><a href="">Hướng dẫn chơi game</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
---
+++ roomy.php
@@ -0,0 +1,120 @@
+<?php @include('header.php') ?>
+
+<table align="center">
+    <tr>
+        <td align="center" height="80px">[ ROOMY EVENT ]</td>
+    </tr>
+    <tr>
+        <td height="80px"></td>
+    </tr>
+    <tr>
+        <td align="center">
+            <table width="300">
+                <tr>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/1r.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' One '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/1b.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' One '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/1l.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' One '>
+                    <td>
+                </tr>
+                <tr>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/2r.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Two '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/2b.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Two '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/2l.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Two '>
+                    <td>
+                </tr>
+                <tr>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/3r.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Three '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/3b.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Three '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/3l.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Three '>
+                    <td>
+                </tr>
+                <tr>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/4r.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Four '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/4b.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Four '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/4l.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Four '>
+                    <td>
+                </tr>
+                <tr>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/5r.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Five '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/5b.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Five '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/5l.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Five '>
+                    <td>
+                </tr>
+                <tr>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/6r.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Six '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/6b.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Six '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/6l.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Six '>
+                    <td>
+                </tr>
+                <tr>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/7r.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Seven '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/7b.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Seven '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/7l.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Seven '>
+                    <td>
+                </tr>
+                <tr>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/8r.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Eight '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/8b.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Eight '>
+                    <td>
+                    <td width='50' align='center' height='60'>
+                        <img src='images/8l.jpg' border='0' class='imgDisplay' style='cursor:pointer;' title=' Eight '>
+                    <td>
+                </tr>
+            </table>
+        </td>
+    <tr>
+        <td height="80"></td>
+    </tr>
+    <tr>
+        <td align="center">
+            <font color="Crimson">
+                <b>
+                    [ <a class="refresh" href="">Refresh</a> ]
+                </b>
+            </font>
+        </td>
+    </tr>
+    <tr>
+        <td height="80"></td>
+    </tr>
+
+
+
